7b01c379f088210aa0b00f5e3f877dfabdf558af
Am-Coder <mishraaman2210@gmail.com>
2022-03-13 14:16:38 +0530

    changes: scoped onecdot package added

--- src/components/OnecLogin.js
+++ src/components/OnecLogin.js
@@ -1,35 +1,34 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
-import Onec from "onec-sdk";
+import Onec from "@onecdot/auth";
 
 class OnecLogin extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        isLoggedIn: false,
-        userAddress: "",
-      };
-    }
-    render() {
-      return (
-        <div>
-            {this.state.isLoggedIn==false ? 
-          <Button variant="primary" onClick={
-            () => { 
-                Onec.auth.withMetamask().then((response) => {
-                    this.setState({isLoggedIn: true, userAddress: response.user.address});
-                    console.log(response)
-                }).catch((error) => {console.log(error)});
-            }
-          }>Login with Metamask</Button>:null}
-        <div>
-            {this.state.isLoggedIn ? <div><h3>Login Successful</h3> <p>User Address: {this.state.userAddress}</p></div> : null}
-        </div>
-        </div>
-
-      );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+      userAddress: "",
+    };
   }
+  render() {
+    return (
+      <div>
+        {this.state.isLoggedIn == false ?
+          <Button variant="primary" onClick={
+            () => {
+              Onec.auth.withMetamask().then((response) => {
+                this.setState({ isLoggedIn: true, userAddress: response.user.address });
+                console.log(response)
+              }).catch((error) => { console.log(error) });
+            }
+          }>Login with Metamask</Button> : null}
+        <div>
+          {this.state.isLoggedIn ? <div><h3>Login Successful</h3> <p>User Address: {this.state.userAddress}</p></div> : null}
+        </div>
+      </div>
 
-  export default OnecLogin;
-  
+    );
+  }
+}
+
+export default OnecLogin;
